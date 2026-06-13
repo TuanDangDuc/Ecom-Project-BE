2b2e74ca3ef1690830a176e8732bc041b7d3b40f
NewbieMt-std <anhtuanmai706@gmail.com>
2026-06-13 11:57:21 +0700

    fix database.php

--- app/core/Database.php
+++ app/core/Database.php
@@ -6,16 +6,8 @@ class Database
 
     public static function connection()
     {
-       if (self::$connection === null) {
-<<<<<<< HEAD
-<<<<<<< HEAD
+        if (self::$connection === null) {
             $config = require __DIR__ . './../../config/dbConf.php';
-=======
-            $config = require __DIR__ . '/../../config/dbConf.php';
->>>>>>> 2cd2577 (add ProductType module)
-=======
-            $config = require __DIR__ . '/../../config/dbConf.php';
->>>>>>> cbc3ef86c29f0e36ad96e909a6ca7ad48579114e
             $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset=utf8mb4";
             try {
                 self::$connection = new PDO($dsn, $config['user'], $config['pass']);
