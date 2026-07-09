cbf1e3a7decb1b219d95df6d56483815a2dfbdcf
TuanDangDuc <tdang9078@gmail.com>
2026-07-09 10:36:22 +0700

    fix

--- config/redisConf.php
+++ config/redisConf.php
@@ -2,7 +2,6 @@
 use Predis\Client;
 
 return [
-    'scheme' => 'tcp',
     'host' => $_ENV['REDIS_HOST'] ?? getenv('REDIS_HOST') ?: 'redis',
     'port' => (int)($_ENV['REDIS_PORT'] ?? getenv('REDIS_PORT') ?: 6379),
 ];
