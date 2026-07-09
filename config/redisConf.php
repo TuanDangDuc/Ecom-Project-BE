7c309fcc450b79ed0368eab284869fc5306f83b5
nonamemiurin-png <nonamemiurin@gmail.com>
2026-07-09 10:21:12 +0700

    Revert "fix: use docker redis service config"
    This reverts commit af2708822fb3348f42e4a428591c56c9be29cb72.

--- config/redisConf.php
+++ config/redisConf.php
@@ -1,16 +1,8 @@
 <?php
 use Predis\Client;
 
-$env = static function (string $key, string $default): string {
-    $value = $_ENV[$key] ?? getenv($key);
-    if ($value === false || $value === null || $value === '') {
-        return $default;
-    }
-    return (string)$value;
-};
-
 return [
     'scheme' => 'tcp',
-    'host' => $env('REDIS_HOST', '127.0.0.1'),
-    'port' => (int)$env('REDIS_PORT', '6379'),
+    'host' => getenv('REDIS_HOST') ?: '127.0.0.1',
+    'port' => (int)(getenv('REDIS_PORT') ?: 6379),
 ];
